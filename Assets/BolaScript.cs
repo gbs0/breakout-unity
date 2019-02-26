034a3b92c472f9a19b7f751786fcdea6c9c6f9d6
gabriel schiavo <gabriel@lewagon.org>
2019-02-26 19:42:38 -0300

    fix ball script

--- Assets/BolaScript.cs
+++ Assets/BolaScript.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class BolaScript : MonoBehaviour
 {
+    Rigidbody2D rb;         // Set the variable type
+
     // Start is called before the first frame update
     void Start()
     {
-        RigidBody2D rb; // Set the variable type and 
-        rb = GetComponent<RigidBody2D>();             // pick RigidBody from game object
+        rb = GetComponent<Rigidbody2D>();             // Pick RigidBody from game object
         //rb = new.RigidBody2D();                     // Init a new RigidBody on game object 
         Vector2 force = new Vector2(0, -200);         // Add x and y paramaters to Vector2 Variable
         rb.AddForce(force);                           // Call the method for addForce on RigidBody
