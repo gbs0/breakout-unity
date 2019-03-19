da8aea110d1a6c8293b14eea65f49f309c537815
gabriel schiavo <gabriel@lewagon.org>
2019-03-19 12:11:42 -0300

    update project

---
+++ Assets/DerrotaScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DerrotaScript : MonoBehaviour
+{	
+
+	Rigidbody2D rb;             // Set the variable type
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody2D>(); 
+	}
+	void OnTriggerEnter2D(Collider2D colisor)
+	{
+		print(colisao.gameObject.tag);
+	}	
+
+	void OnTriggerStay2D(Collider2D colisor)
+	{
+
+	}
+
+	void OnTriggerExit2D(Collider2D colisor)
+	{
+
+	}
+
+}
