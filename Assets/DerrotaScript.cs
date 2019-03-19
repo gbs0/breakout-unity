4616e2a1fdeb6921f5933ed5e29581bd89b91238
gabriel schiavo <gabriel@lewagon.org>
2019-03-19 12:25:02 -0300

    add defeat condition

--- Assets/DerrotaScript.cs
+++ Assets/DerrotaScript.cs
@@ -6,7 +6,7 @@ public class DerrotaScript : MonoBehaviour
 {	
 
 	Rigidbody2D rb;             // Set the variable type
-
+	int hit_times = 0;
 	void Start() 
 	{
 		rb = GetComponent<Rigidbody2D>(); 
@@ -24,6 +24,17 @@ public class DerrotaScript : MonoBehaviour
 	void OnTriggerExit2D(Collider2D colisor)
 	{
 
+	}
+
+	void Die(int hit_times)
+	{
+		if (hit_times > 3) {
+			if(Time.timeScale == 1)
+					Time.timeScale == 0;
+			else
+				Time.timeScale == 1;
+		}
+		// freeze game
 	}
 
 }
