5ec1c18698c87019a4e4eb5bc8e974b6b84496aa
gabriel schiavo <gabriel@lewagon.org>
2019-03-19 16:41:51 -0300

    fix bug on if statement

--- Assets/DerrotaScript.cs
+++ Assets/DerrotaScript.cs
@@ -6,16 +6,31 @@ public class DerrotaScript : MonoBehaviour
 {	
 
 	Rigidbody2D rb;             // Set the variable type
-	int hit_times = 0;
+	int hit_times = 3;
 	bool isGoingUp = true;
 
 	void Start() 
 	{
 		rb = GetComponent<Rigidbody2D>(); 
 	}
+
+	void Update()
+	{
+		// this should be an bool condition
+		if (hit_times >= 3) {
+			Debug.Log("Defeat!");
+		// 	if(Time.timeScale == 1)
+		// 			Time.timeScale == 0;
+		// 	} else {
+		// 		Time.timeScale == 1;
+		}
+	}
+
 	void OnTriggerEnter2D(Collider2D colisor)
 	{
 		print(colisor.gameObject.tag);
+		// if tag == bola 
+		// hit_times++;
 	}	
 
 	void OnTriggerStay2D(Collider2D colisor)
@@ -27,15 +42,5 @@ public class DerrotaScript : MonoBehaviour
 	{
 
 	}
-
-	void Update()
-	{
-		// this should be an bool condition
-		if (isGoingUp) {
-			if(Time.timeScale == 1)
-					Time.timeScale == 0;
-			else
-				Time.timeScale == 1;
-		}
-	}
 }
+
