8889f634cc7025434a0ef82c93ff80b726a81b64
gabriel schiavo <gabriel@lewagon.org>
2019-03-19 22:04:08 -0300

    add player axis rotate control

--- Assets/JogadorScript.cs
+++ Assets/JogadorScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JogadorScript : MonoBehaviour
 {   
-    Transform t;                                        // Add data type for t variable
+    Transform t;                                      // Add data type for t variable
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +24,15 @@ public class JogadorScript : MonoBehaviour
             t.Translate(-0.1f, 0, 0);
         }
 
+        if (Input.GetKey(KeyCode.Q)) {
+            t.Rotate(0, 0, 6f);
+        }
+
+        if (Input.GetKey(KeyCode.E)) {
+            t.Rotate(0, 0, -6f);
+        }
+        
+        // Restrict Player Area
         Vector3 posicao = t.position;
         // if (posicao.x > 2.9f) posicao.x = 2.9f;
         // if (posicao.x < -2.9f) posicao.x = -2.9f;
