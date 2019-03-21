39c95f99d58ff513c5728324391f768b7daed559
gabriel schiavo <gabriel@lewagon.org>
2019-03-21 17:19:29 -0300

    Plug text gameObject

--- Assets/UIScript.cs
+++ Assets/UIScript.cs
@@ -7,9 +7,15 @@ using UnityEngine.UI;
 public class UIScript : MonoBehaviour
 {
 		public Text lifes;
+		public Text time;
 
     public void ChangeLife(int value)
     {
     	lifes.text = "Lifes: " + value;
+    }
+
+    void Update()
+    {
+    	
     }
 }
