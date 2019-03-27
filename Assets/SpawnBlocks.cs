3161dfb9f0dca9c108db26d38efaad4e5df3bdfc
gabriel schiavo <gabriel@lewagon.org>
2019-03-26 21:44:23 -0300

    init a superblock

--- Assets/SpawnBlocks.cs
+++ Assets/SpawnBlocks.cs
@@ -30,7 +30,9 @@ public class SpawnBlocks : MonoBehaviour
     	
     	for(int column = 0; column < 3; column++) {
     		for (int row = 0; row < 5; row++) {
-	    		GameObject copia = Instantiate(block, transform);
+	    		GameObject copia;
+	    		if (1 < level/2) copia = Instantiate(superblock, transform);
+	    		else copia = Instantiate(block, transform);
 	    		Vector2 newPosition = genesisBlock;
 	    		newPosition.x += 1.5f * row;
 	    		newPosition.y -= 0.8f * column;
